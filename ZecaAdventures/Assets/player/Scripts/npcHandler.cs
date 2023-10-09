8c04758e5c1cf6454abdcc32c5d9f59adc24481d
Lucas Fernandes <lucasrochafernandes20@gmail.com>
2023-10-09 04:13:35 -0300

    feat: npc collect and drop

--- ZecaAdventures/Assets/player/Scripts/npcHandler.cs
+++ ZecaAdventures/Assets/player/Scripts/npcHandler.cs
@@ -1,27 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class npcHandler : MonoBehaviour
 {
     public Transform seatPosition;
+    public Transform dropPosition;
+    public Vector3 seatOffset;
     public bool carryingNpc;
     private GameObject npcOnCarry;
+
+    
     
     
     private void Start() {
         carryingNpc = false;
+    }   
+
+
+    private void Update() {
+        if(carryingNpc){
+            carryNpc(npcOnCarry);
+        }
     }
 
     private void OnTriggerEnter(Collider other) {
         if(other.gameObject.tag == "npc"){
             if(!carryingNpc){
-                print("devNote: got an npc");
                 carryingNpc = true;
                 npcOnCarry = other.gameObject;
                 getNpc(other.gameObject);
             }
         }
+        else if(other.gameObject.tag == "dropZone"){
+            dropNpc();
+        }
+    }
+
+
+    private void carryNpc(GameObject npc){
+        
+            npc.transform.position = seatPosition.position + seatOffset;
+        
     }
 
     private void getNpc(GameObject npc){
@@ -30,7 +51,11 @@ public class npcHandler : MonoBehaviour
         print("npc position updated");
     }
 
-    private void dropNpc(GameObject npc){
+    private void dropNpc(){
+        // get npc inside the school
+        npcOnCarry.transform.position = dropPosition.transform.position;
+        // reset npc handler
+        carryingNpc = false;
 
     }
 
